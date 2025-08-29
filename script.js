3d4345df279ca7481d4ce1c5aae4615155d51cb0
Edgartor321 <edgar18tor@gmail.com>
2025-08-28 21:51:04 -0600

    Version1.0

---
+++ script.js
@@ -0,0 +1,15 @@
+function calcular(){
+    var magnitud=parseFloat(document.getElementById("magnitud").value);
+    var ang = parseFloat(document.getElementById("angulo").value);
+
+    if(isNaN(magnitud)||isNaN(ang)){
+        document.getElementById("resultado").innerHTML="Los valores ingresados no son validos"
+        return;
+    }
+
+    var rad=(ang*Math.PI)/180;
+    var x=magnitud*Math.cos(rad);
+    var y=magnitud*Math.sin(rad);
+
+    document.getElementById("resultado").innerHTML="Componenete en X:"+x+"<br>Componente en Y:"+y;
+}
